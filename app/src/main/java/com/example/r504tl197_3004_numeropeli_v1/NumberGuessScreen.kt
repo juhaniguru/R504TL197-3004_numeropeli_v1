35eb2809483a43ca3d674aff5d8dc5ff2824a625
juhani.kuru <juhanialeksikuru@gmail.com>
2026-03-17 18:10:26 +0200

    add ui components

--- app/src/main/java/com/example/r504tl197_3004_numeropeli_v1/NumberGuessScreen.kt
+++ app/src/main/java/com/example/r504tl197_3004_numeropeli_v1/NumberGuessScreen.kt
@@ -1,9 +1,24 @@
 package com.example.r504tl197_3004_numeropeli_v1
 
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.r504tl197_3004_numeropeli_v1.ui.theme.R504TL1973004_numeropeli_v1Theme
 import kotlin.random.Random
 
 @Composable
@@ -20,9 +35,50 @@ fun NumberGuessScreenRoot(modifier: Modifier = Modifier) {
     NumberGuessScreen(state = state.value)
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NumberGuessScreen(modifier: Modifier = Modifier, state: NumberGuessState) {
 
+    Scaffold(topBar = {
+        TopAppBar(title = {
+            Text("Numeropeli")
+        })
+    }) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
 
-    
+                TextField(value = state.number, onValueChange = { newText ->
+                    Log.d("juhanitestaa", newText)
+                })
+
+                Button(onClick = {}) {
+                    Text("Arvaa")
+                }
+
+                Text(state.guessText)
+
+                Button(onClick = {}) {
+                    Text("Uusi peli")
+                }
+
+            }
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun NumberGuessScreenPreview() {
+    R504TL1973004_numeropeli_v1Theme {
+        NumberGuessScreen(state = NumberGuessState())
+    }
 }
